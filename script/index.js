85764eeda70373c90674fe7c1cc8f897ada4aa20
ValeriaCommatteo <valc2012@hotmail.com>
2023-03-06 14:15:08 -0300

    creacion

---
+++ script/index.js
@@ -0,0 +1,27 @@
+const tarjetasHome = document.getElementById("cards")
+
+let tarjetasCargadas = crearTarjetas(data.events);
+
+function crearTarjetas(lista){
+    let tarjetas = "";
+
+    for (let eventos of lista){
+      tarjetas +=`
+      <div class="card event__card p-2 m-4 border-0 text-center">
+      <div class="row no-gutters">
+        <div class="col-sm-4" style="margin-left: 90px">
+          <img class="card-img rounded" src=" ${eventos.image} width="100" height="200">
+        </div>
+        <div class="col-sm-6"  style="padding-top: 50px">
+          <div class="card-body">
+            <h5 class="card-title">${eventos.name}</h5>
+            <p class="card-text">${eventos.description}</p>
+          </div>
+        </div>
+      </div>
+    </div> `
+  }
+    return tarjetas;
+}
+
+tarjetasHome.innerHTML = tarjetasCargadas;
